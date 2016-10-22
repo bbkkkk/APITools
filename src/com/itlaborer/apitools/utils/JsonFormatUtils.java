152ecedefc5cdd6367e95f07532186fb948b3dbd
793554262@qq.com <793554262@qq.com>
2016-10-22 10:40:02 +0800

    修复json格式化显示问题

--- src/com/itlaborer/apitools/utils/JsonFormatUtils.java
+++ src/com/itlaborer/apitools/utils/JsonFormatUtils.java
@@ -5,10 +5,14 @@ public class JsonFormatUtils {
 	public static String Format(String jsonStr) {
 		int level = 0;
 		StringBuffer jsonForMatStr = new StringBuffer();
+		char pre1 = 0;
+		char next = 0;
 		for (int i = 0; i < jsonStr.length(); i++) {
-			char pre1 = 0;
 			if (i > 0) {
 				pre1 = jsonStr.charAt(i - 1);
+			}
+			if (i < jsonStr.length() - 1) {
+				next = jsonStr.charAt(i + 1);
 			}
 			char c = jsonStr.charAt(i);
 			if (level > 0 && '\n' == jsonForMatStr.charAt(jsonForMatStr.length() - 1)) {
@@ -21,7 +25,9 @@ public class JsonFormatUtils {
 				level++;
 				break;
 			case ',':
-				if (pre1 == '"' || Character.isDigit(pre1)) {
+				if ((pre1 == '"' && next == '"') || (Character.isDigit(pre1) && next == '"')
+						|| ((jsonForMatStr.length() > 3)
+								&& (jsonForMatStr.substring(jsonForMatStr.length() - 4) .equals("null")) && next == '"')) {
 					jsonForMatStr.append(c + "\n");
 				} else {
 					jsonForMatStr.append(c);
