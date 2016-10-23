2108ca596ffc7e6723231de445f5044e5126d472
793554262@qq.com <793554262@qq.com>
2016-10-23 18:25:10 +0800

    更新Json格式化工具类

--- src/com/itlaborer/apitools/utils/JsonFormatUtils.java
+++ src/com/itlaborer/apitools/utils/JsonFormatUtils.java
@@ -1,9 +1,16 @@
 package com.itlaborer.apitools.utils;
 
+/**
+ * Json格式化输出工具，对于遵守 RFC4627约定的Json串具有很好的兼容性
+ * 
+ * @author liudewei[793554262@qq.com]
+ * @version 1.3
+ * @since 1.0
+ */
 public class JsonFormatUtils {
 
 	public static String Format(String jsonStr) {
-		int level = 0;
+		int tablevel = 0;
 		StringBuffer jsonForMatStr = new StringBuffer();
 		char pre1 = 0;
 		char next = 0;
@@ -15,19 +22,25 @@ public class JsonFormatUtils {
 				next = jsonStr.charAt(i + 1);
 			}
 			char c = jsonStr.charAt(i);
-			if (level > 0 && '\n' == jsonForMatStr.charAt(jsonForMatStr.length() - 1)) {
-				jsonForMatStr.append(GetLevelStr(level));
+			if (tablevel > 0 && '\n' == jsonForMatStr.charAt(jsonForMatStr.length() - 1)) {
+				jsonForMatStr.append(GetLevelStr(tablevel));
 			}
 			switch (c) {
 			case '{':
 			case '[':
 				jsonForMatStr.append(c + "\n");
-				level++;
+				tablevel++;
 				break;
 			case ',':
+				// 在这里需要严格判断是否需要换行
 				if ((pre1 == '"' && next == '"') || (Character.isDigit(pre1) && next == '"')
 						|| ((jsonForMatStr.length() > 3)
-								&& (jsonForMatStr.substring(jsonForMatStr.length() - 4) .equals("null")) && next == '"')) {
+								&& (jsonForMatStr.substring(jsonForMatStr.length() - 4).equals("null")) && next == '"')
+						|| ((jsonForMatStr.length() > 3)
+								&& (jsonForMatStr.substring(jsonForMatStr.length() - 4).equals("true")) && next == '"')
+						|| ((jsonForMatStr.length() > 3)
+								&& (jsonForMatStr.substring(jsonForMatStr.length() - 5).equals("false"))
+								&& next == '"')) {
 					jsonForMatStr.append(c + "\n");
 				} else {
 					jsonForMatStr.append(c);
@@ -36,8 +49,8 @@ public class JsonFormatUtils {
 			case '}':
 			case ']':
 				jsonForMatStr.append("\n");
-				level--;
-				jsonForMatStr.append(GetLevelStr(level));
+				tablevel--;
+				jsonForMatStr.append(GetLevelStr(tablevel));
 				jsonForMatStr.append(c);
 				break;
 			default:
@@ -48,11 +61,11 @@ public class JsonFormatUtils {
 		return jsonForMatStr.toString();
 	}
 
-	private static String GetLevelStr(int level) {
-		StringBuffer levelStr = new StringBuffer();
-		for (int levelI = 0; levelI < level; levelI++) {
-			levelStr.append("\t");
+	private static String GetLevelStr(int tablevel) {
+		StringBuffer tablevelStr = new StringBuffer();
+		for (int level = 0; level < tablevel; level++) {
+			tablevelStr.append("\t");
 		}
-		return levelStr.toString();
+		return tablevelStr.toString();
 	}
 }
