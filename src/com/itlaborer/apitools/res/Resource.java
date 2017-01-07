96655f7fb43127ddd13e21a28511fcf8f66cb9bd
793554262@qq.com <793554262@qq.com>
2017-01-07 21:36:17 +0800

    修正错误

--- src/com/itlaborer/apitools/res/Resource.java
+++ src/com/itlaborer/apitools/res/Resource.java
@@ -15,7 +15,7 @@ public class Resource {
 	public static String BLOG = "http://www.itlaborer.com";
 	public static String CONFIG = "#主配置文件\r\n#请按照参数名进行相关配置\r\n#服务器地址,此参数已不生效，请将服务器地址配置到接口文档json文件中的serverlist,多个服务器请以|分割"
 			+ "\r\n#自动装载的API列表文件\r\napilist=api-xinzhiweather.json\r\n#自动装载的错误码列表文件-仅支持恒生FUNDAPI\r\nreturncodefile"
-			+ "=xxx.properties\r\n#历史记录条数\r\nhsitorysum=30";
+			+ "=xxx.properties\r\n#历史记录条数\r\nhistorysum=30";
 	public static String EXPLAIN = "这是一个适合HTTP服务的请求工具，可以快速的填充参数，拉取接口返回的结果,如果你"
 			+ "不想每次测试接口的时候都手动输入大量的参数,可以预先编辑好接口和参数列表,这样工具启动的时候" + "可以自动加载预先定义好的接口信息,这一切都是如此的简单,你只需要掌握"
 			+ "简单的JSON格式书写规则即可";
